87a277949dda2090be9769f31ca1a0687728c796
paulwongx <20178251+paulwongx@users.noreply.github.com>
2020-08-05 22:01:36 -0400

    Changed to version 0.33 of react-bootstrap

--- src/components/LoaderButton.js
+++ src/components/LoaderButton.js
@@ -1,6 +1,5 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
-import { Glyphicon } from "react-bootstrap";
+import { Button, Glyphicon } from "react-bootstrap";
 import "./LoaderButton.css";
 
 export default function LoaderButton({
